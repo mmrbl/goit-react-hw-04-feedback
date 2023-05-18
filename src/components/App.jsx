fd6df683a45223d3a2a27cd8f40361167aef1a69
Ihor <itotonk1@gmail.com>
2023-05-18 19:56:44 +0300

    refactoring classes for functional components with hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useReducer } from 'react';
 import { Container } from './App.styled';
 import FeedbackOptions from './Feedback/FeedbackOptions';
 import Notification from './Feedback/Notification';
@@ -6,48 +6,55 @@ import Section from './Feedback/Section';
 import Statistics from './Feedback/Statistics';
 
 
-export class App extends Component{
+function countRating(state, actions){
+  switch (actions) {
+    case 'good':
+      return { ...state, good: state.good + 1 }
+    
+    case 'neutral':
+      return { ...state, neutral: state.neutral + 1 }
+    
+    case 'bad':
+      return {...state, bad: state.bad + 1}
+  
+    default:
+      break;
+  }
+}
 
-  state = {
+
+export function App() {
+  const [state, dispatch] = useReducer(countRating, {
     good: 0,
     neutral: 0,
     bad: 0,
+  });
+
+  
+  const {good, neutral, bad} = state
+  function countTotalFeedback() {
+    return good + neutral + bad
+  }
+  
+  function countPositiveFeedbackPercentage() {
+    return Math.floor(good*100/countTotalFeedback())
   }
 
-   leaveFeedback = stateKey => () => {
-    this.setState(prevState => {
-      return { [stateKey]: prevState[stateKey] + 1 };
-    });
-  };
-
-    countTotalFeedback() {
-    return this.state.good + this.state.neutral + this.state.bad
-  }
-
-  countPositiveFeedbackPercentage() {
-    return Math.floor(this.state.good*100/this.countTotalFeedback())
-  }
-
-  render() {
-
-    const {good, neutral, bad} = this.state
-    return (
+  return (
       <Container>
-      
-
       <Section title="Please leave feedback">
          <FeedbackOptions
-            options={Object.keys(this.state)}
-            onFeedbackClick={this.leaveFeedback}
+          options={Object.keys(state)}
+            onFeedbackClick={(action) => dispatch(action)}
           />
         </Section>
-        {this.countTotalFeedback() > 0 ? (
+        {countTotalFeedback() > 0 ? (
           <Section title="Staticstics">
-            <Statistics good={good} neutral={neutral} bad={bad} total={this.countTotalFeedback()} positivePercentage={this.countPositiveFeedbackPercentage()}>
+            <Statistics good={good} neutral={neutral} bad={bad} total={countTotalFeedback()} positivePercentage={countPositiveFeedbackPercentage()}>
            </Statistics>
       </Section>): <Notification message="There is no feedback"/>}
-      
     </Container>
     )
-  }
 }
+
+
--- src/components/Feedback/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/Feedback/FeedbackOptions/FeedbackOptions.jsx
@@ -2,11 +2,11 @@ import PropTypes from 'prop-types';
 import shortid from "shortid";
 import { FeedbackButton, FeedbackElement, FeedbackList } from "./FeedbackOptions.styled";
 
-const FeedbackOptions = ({ options, onFeedbackClick }) => (
+const FeedbackOptions = ({ options, onFeedbackClick, dispatch }) => (
     <FeedbackList>
       {options.map((option) => (
         <FeedbackElement key={shortid.generate()}>
-          <FeedbackButton type='button' onClick={onFeedbackClick(option)}>{option[0].toUpperCase() + option.slice(1)}</FeedbackButton>
+          <FeedbackButton type='button' onClick={() => onFeedbackClick(option, dispatch)}>{option[0].toUpperCase() + option.slice(1)}</FeedbackButton>
         </FeedbackElement>
       ))}
 
@@ -19,4 +19,5 @@ export default FeedbackOptions
 FeedbackOptions.propTypes = {
   options: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
   onFeedbackClick: PropTypes.func.isRequired,
+  dispatch: PropTypes.func,
 }
